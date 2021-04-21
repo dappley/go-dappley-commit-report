fa81192952316670fc9fa8bb7eba53c89143afc3
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-04-21 13:26:09 -0700

    Testing

--- EmailOnPush.go
+++ EmailOnPush.go
@@ -17,10 +17,18 @@ func main() {
 
 	dev_committer, dev_email, dev_fail := compose("develop")
 	master_committer, master_email, master_fail := compose("master")
+
+	fmt.Println("Branch: develop")
+	fmt.Println("Committer:", dev_committer, "\n")
+	fmt.Println(dev_email)
+	
+	fmt.Println("Branch: master")
+	fmt.Println("Committer:", master_committer, "\n")
+	fmt.Println(master_email)
 	
 	//send develop branch info
 	if dev_fail {
-		send(dev_email, "develop", dev_committer, senderEmail, senderPasswd)
+		//send(dev_email, "develop", dev_committer, senderEmail, senderPasswd)
 		fmt.Println("Email sent to develop branch committer:", dev_committer)
 	} else {
 		fmt.Println("No fail case on develop branch!")
@@ -28,7 +36,7 @@ func main() {
 
 	//send master branch info
 	if master_fail {
-		send(master_email, "master", master_committer, senderEmail, senderPasswd)
+		//send(master_email, "master", master_committer, senderEmail, senderPasswd)
 		fmt.Println("Email sent to master branch committer:", master_committer)
 	} else {
 		fmt.Println("No fail case on master branch!")
